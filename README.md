7979d2fb3679a447573b77cacb10fa5f82cf0f2a
Mike Wilcox <mwilcox56@gmail.com>
2016-01-11 22:31:11 -0500

    demo

---
+++ README.md
@@ -0,0 +1,18 @@
+d3-react
+========
+
+simple d3 react cirlces
+
+![circles](./circles.png)
+
+# Run Locally
+
+```bash
+python -m SimpleHTTPServer <port>
+```
+
+or using [serve](https://www.npmjs.com/package/serve)
+
+```bash
+serve
+```
---
+++ index.js
@@ -0,0 +1,140 @@
+/* Chart d3 class */
+var Chart = function(element, props, state) {
+  this.element = element;
+  this.props = props;
+  this.state = state;
+}
+
+/* Chart init */
+Chart.prototype.init = function() {
+  var svg = d3.select(this.element).append('svg')
+    .attr('class', 'd3')
+    .attr('width', this.props.width)
+    .attr('height', this.props.height);
+
+  svg.append('g')
+    .attr('class', 'd3-points');
+
+  this.update();
+}
+
+/* Scale the data */
+Chart.prototype._scales = function() {
+  if (!this.state.domain) {
+    return null;
+  }
+
+  var width = this.element.offsetWidth;
+  var height = this.element.offsetHeight;
+
+  var x = d3.scale.linear()
+    .range([0, width])
+    .domain(this.state.domain.x);
+
+  var y = d3.scale.linear()
+    .range([height, 0])
+    .domain(this.state.domain.y);
+
+  var z = d3.scale.linear()
+    .range([5, 20])
+    .domain([1, 10]);
+
+  return {x: x, y: y, z: z};
+}
+
+/* Update the data */
+Chart.prototype.update = function() {
+  var scales = this._scales();
+  this._drawPoints(scales);
+}
+
+/* Draw the data */
+Chart.prototype._drawPoints = function(scales) {
+  var g = d3.select(this.element).selectAll('.d3-points');
+
+  var color = d3.scale.category20();
+
+  var point = g.selectAll('.d3-point')
+    .data(this.state.data, function(d) { return d.id; });
+
+  point.enter().append('circle')
+    .attr('class', 'd3-point')
+    .style('fill', function(d,i) { return color(i); });
+
+  point.attr('cx', function(d) { return scales.x(d.x); })
+    .attr('cy', function(d) { return scales.y(d.y); })
+    .attr('r', function(d) { return scales.z(d.z); });
+
+  point.exit().remove();
+};
+
+/* Main Container */
+var App = React.createClass({
+
+  getInitialState: function() {
+    return {
+      data: [
+          { id: '1', x: 3, y: 30, z: 4 },
+          { id: '2', x: 6, y: 32, z: 6 },
+          { id: '3', x: 9, y: 35, z: 4 },
+          { id: '4', x: 11, y: 38, z: 9 },
+          { id: '5', x: 14, y: 41, z: 2 },
+          { id: '6', x: 16, y: 35, z: 11 },
+          { id: '7', x: 19, y: 38, z: 7 },
+          { id: '8', x: 21, y: 47, z: 9 },
+          { id: '9', x: 23, y: 41, z: 7 },
+          { id: '10', x: 25, y: 30, z: 4 },
+      ],
+      domain: {
+        x: [0, 30], 
+        y: [0, 100]
+      }
+    }
+  },
+
+  render: function() {
+    return React.createElement('div', null, [
+      React.createElement('h1', { key: 'header' }, 'd3-react'),
+      React.createElement(ChartComponent, { 
+        key: 'chart-component',
+        data: this.state.data,
+        domain: this.state.domain
+      })
+    ]);
+  }
+});
+
+/* Chart Component */
+var ChartComponent = React.createClass({
+
+  chart: {},
+
+  componentDidMount: function() {
+    var element = ReactDOM.findDOMNode(this);
+    this.chart = new Chart(
+      element, 
+      {
+        width: '100%',
+        height: '500px'
+      },
+      {
+        data: this.props.data,
+        domain: this.props.domain
+      }
+    );
+
+    this.chart.init();
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', { className: 'chart-component' })
+    );
+  }
+});
+
+/* Bootstrap */
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById('root')
+);
